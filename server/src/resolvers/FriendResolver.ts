6ea56e0c0a1f00f03618fab7f0489aae1588fa8e
carlosrrdev <rr.carlos.dev@gmail.com>
2022-10-28 17:39:30 -0500

    Add auth to resolver

--- server/src/resolvers/FriendResolver.ts
+++ server/src/resolvers/FriendResolver.ts
@@ -5,6 +5,7 @@ import { AppDataSource } from "../db/connection";
 
 @Resolver()
 export class FriendResolver {
+  // Get friends by sourceId
   @Query(() => [User])
   getFriendsBySourceId(@Arg("sourceId") sourceId: number): Promise<User[]> {
     return AppDataSource.getRepository(Friendship).query(
@@ -24,6 +25,11 @@ export class FriendResolver {
     @Arg("sourceId") sourceId: number,
     @Arg("targetId") targetId: number
   ): Promise<Boolean> {
+    // Check if target user exists
+    const target = await User.findOne({ where: { id: targetId } });
+    if (!target) {
+      throw new Error("Target user does not exist");
+    }
     const friend = Friendship.create({
       sourceId,
       targetId,
